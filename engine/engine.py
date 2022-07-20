dd2b6837f46b4cd8b280694d60f6f46716f7c7ff
limboden <99365731+limboden@users.noreply.github.com>
2022-07-20 18:35:03 -0400

    task 1 complete
    started on 2

---
+++ engine/engine.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from car import Car
+
+class Engine():
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+
+
+    @abstractmethod
+    def needs_service():
+        pass
